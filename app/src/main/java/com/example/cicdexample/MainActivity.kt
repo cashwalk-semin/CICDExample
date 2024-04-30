048cc090b9ec06f2742b11ba4290e6805ed68dbe
오세민 <oh.semin@cashwalk.io>
2024-04-30 17:09:34 +0900

    add list2

--- app/src/main/java/com/example/cicdexample/MainActivity.kt
+++ app/src/main/java/com/example/cicdexample/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.cicdexample.ui.theme.CICDExampleTheme
 
 class MainActivity : ComponentActivity() {
     val list = listOf("seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang")
+    val list2 = listOf("seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang","seminzzang")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
