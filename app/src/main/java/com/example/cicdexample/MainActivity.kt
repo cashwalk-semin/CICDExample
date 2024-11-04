8e39dc6d1fb1e3106739859faa8af8a7ef13f3e0
오세민 <oh.semin@cashwalk.io>
2024-11-04 18:00:23 +0900

    seokgyu~

--- app/src/main/java/com/example/cicdexample/MainActivity.kt
+++ app/src/main/java/com/example/cicdexample/MainActivity.kt
@@ -72,7 +72,7 @@ fun Greeting(
     modifier: Modifier = Modifier,
 ) {
     Text(
-        text = "Hello $name!!!!!",
+        text = "Hello $name!!!!!!!!!!!!",
         modifier = modifier,
     )
 }
